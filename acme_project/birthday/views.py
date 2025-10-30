e57e4e21587c68bdfae7b87cc7d1bc293d690ff6
m2dyt <roman16050811@mail.ru>
2025-10-30 11:26:11 +0300

    first

---
+++ acme_project/birthday/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from .forms import BirthdayForm
+
+def birthday(request):
+    form = BirthdayForm
+    context = {'form' : form}
+    return render(request, 'birthday/birthday.html', context)
