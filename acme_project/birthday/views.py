99f7d934199168c5f35f322e4fff7f0afc22904a
m2dyt <roman16050811@mail.ru>
2025-10-31 20:53:40 +0300

    добавлена работа с бд

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from .forms import BirthdayForm
 from .utils import calculate_birthday_countdown
+from .models import Birthday
 
 def birthday(request):
-    form = BirthdayForm(request.GET or None)
+    form = BirthdayForm(request.POST or None)
     context = {'form': form}
     if form.is_valid():
+        form.save()
         birthday_countdown = calculate_birthday_countdown(form.cleaned_data['birthday'])
         context.update({'birthday_countdown' : birthday_countdown})
     return render(request, 'birthday/birthday.html', context) 
+
+def birthday_list(request):
+    birthdays = Birthday
