33c7f439b329338ec10288e45eaa12840caaed34
m2dyt <roman16050811@mail.ru>
2025-11-02 00:33:35 +0300

    редактирование

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -2,9 +2,16 @@ from django.shortcuts import render
 from .forms import BirthdayForm
 from .utils import calculate_birthday_countdown
 from .models import Birthday
+from django.shortcuts import get_object_or_404, render
 
-def birthday(request):
-    form = BirthdayForm(request.POST or None)
+def birthday(request, pk=None):
+    if pk is not None:
+        instace = get_object_or_404(Birthday, pk=pk)
+    else:
+        instace = None
+
+    form = BirthdayForm(request.POST or None, instance=instace)
+    
     context = {'form': form}
     if form.is_valid():
         form.save()
@@ -12,5 +19,9 @@ def birthday(request):
         context.update({'birthday_countdown' : birthday_countdown})
     return render(request, 'birthday/birthday.html', context) 
 
+
+
 def birthday_list(request):
-    birthdays = Birthday
+    birthdays = Birthday.objects.all()
+    context = {"birthdays": birthdays}
+    return render(request, 'birthday/birthday_list.html', context)
